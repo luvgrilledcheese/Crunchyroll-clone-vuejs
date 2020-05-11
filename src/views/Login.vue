245668a26d350aade0eaebe1c00f23791a07d4dc
Guillaume Lachance <glachance@cmaisonneuve.qc.ca>
2020-05-11 11:04:46 -0400

    Modifications Login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,27 +1,52 @@
 <template>
     <div class="container">
         <h2 style="text-align:center">Login</h2>
-        <form>
-            <div class="form-group row">
-                <label for="email" class="col-sm-2 col-form-label">Email</label>
-                <div class="col-sm-10">
-                    <input type="email" class="form-control" id="email" placeholder="Email"/>
+        <form id="frmLogin">
+            <div id="grpEmail" class="form-group row">
+                <label for="email" class="col-3 col-form-label">Email</label>
+                <div class="col-9">
+                    <input
+                        type="email"
+                        class="form-control"
+                        id="email"
+                        placeholder="Email"
+                        required
+                        maxlength="100"
+                    />
+                </div>
+            </div>
+            <div id="grpPassword" class="form-group row">
+                <label for="password" class="col-3 col-form-label">Password</label>
+                <div class="col-9">
+                    <input
+                        type="password"
+                        class="form-control"
+                        id="password"
+                        placeholder="Password"
+                        required
+                        minlength="4"
+                        maxlength="15"
+                    />
                 </div>
             </div>
             <div class="form-group row">
-                <label for="password" class="col-sm-2 col-form-label">Password</label>
-                <div class="col-sm-10">
-                    <input type="password" class="form-control" id="password" placeholder="Password" />
+                <div class="col-3"></div>
+                <div class="col-9">
+                    <button type="submit" v-on:click="Login" class="btn btn-primary">
+                        Login
+                    </button>
                 </div>
             </div>
-            <div class="form-group row">
-                <div class="col-sm-10">
-                    <button type="submit" class="btn btn-primary">Login</button>
-                </div>
-            </div>
-            <div id="diverror" class="row" style="display:none">
-                <div id="msgerror" class="alert alert-danger" style="display:inline-block;margin:0 auto;" role="alert">
-                Une erreur s'est produite.
+            <div class="form-group row" v-if="errorText != ''">
+                <div class="col-3"></div>
+                <div id="diverror" class="col-9">
+                    <div
+                        id="msgerror"
+                        class="alert alert-danger"
+                        style="display:inline-block;margin:0 auto;"
+                        role="alert">
+                        {{errorText}}
+                    </div>
                 </div>
             </div>
         </form>
@@ -29,14 +54,25 @@
 </template>
 
 <script>
-export default {
-    name: "Login",
-    methods: {
-        //est appellée lorsque le bouton Login est cliqué
-        Login : function () {
-            //appeller la méthode /token pour obtenir un bearer token et
-            //permettre l'accès aux méthodes protégées par authentification
+    export default {
+        name: "Login",
+        data: function() {
+            return {
+                errorText: ""
+            };
+        },
+        methods: {
+            //est appellée lorsque le bouton Login est cliqué
+            Login: function() {
+                //appeller la méthode /token pour obtenir un bearer token et
+                //permettre l'accès aux méthodes protégées par authentification
+                //sauvegarder le bearer token dans localStorage
+                this.errorText = "";
+                let valid = document.getElementById("frmLogin").checkValidity();
+                if (valid) {
+                    console.log("formulaire validé");
+                }
+            }
         }
-    }
-};
+    };
 </script>
