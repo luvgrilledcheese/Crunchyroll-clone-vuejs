1ef76f4166d7944f7d1157d14195d8f042d12c92
Richi Tan <e1629525@cmaisonneuve.qc.ca>
2020-05-13 23:09:42 -0400

    TP3-P2

--- src/views/DetailsTvShow.vue
+++ src/views/DetailsTvShow.vue
@@ -1,7 +1,61 @@
 <template>
     <div class="container" v-if="TvShow != null">
-        <!--Complétez le html nécessaire pour l'affichage -->
-        <!--Vous devez avoir complété la méthode GetTvShow -->
+        <div class="row mb-4">
+            <div class="col-5">
+                <img style="max-width: 450px; min-width: 450px;" v-bind:src="TvShow.Image" v-bind:alt="TvShow.Title">
+            </div>
+            <div class="col-7">
+                <div class="row">
+                    <div class="col-11">
+                        <h2 >{{TvShow.Title}}</h2>
+                    </div>
+                    <div class="col-1"></div>
+                    <h5 class="col-12">{{TvShow.Year}}</h5>
+                    <div class="col-3">
+                        <p >{{TvShow.EpisodeCount}} episodes</p>
+                    </div>
+                    <div class="col-2">
+                        <p >{{TvShow.TVParentalGuideline}}</p>
+                    </div>
+                    <div class="col-7">
+                        <p class="text-right">
+                            <span v-for="g in TvShow.Genres" v-if="TvShow.Genres[TvShow.Genres.length - 1] != g">{{g.Name}}, </span>
+                            <span v-else>{{g.Name}}</span>
+                        </p>
+                    </div>
+                    <div class="col-2">
+                        <p >Studio</p>
+                    </div>
+                    <div class="col-10">
+                        <p >{{TvShow.Studio.Name}}</p>
+                    </div><div class="col-12 text-justify">
+                        <p >{{TvShow.Plot}}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-2 col-md-2 col-sm-3 mb-4" v-for="r in TvShow.Roles">
+                <div class="card" style="min-width: 160px; max-width: 160px;">
+                    <img v-bind:src="r.Image" v-bind:alt="r.Name" class="card-img-top mx-auto rounded role-img" style="min-width: 160px; max-width: 160px; min-height: 240px; max-height: 240px;">
+                    <div class="card-body text-center" style="min-height: 140px;">
+                        <p class="card-title small" style="font-weight: bold;">{{r.Name}}</p>
+                        <p class="small">{{r.Character}}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-3 col-md-4 col-sm-6 mb-4" v-for="s in TvShow.Seasons">
+                <div class="card" style="max-width: 250px; min-width: 250px;">
+                    <img v-bind:src="s.Image" v-bind:alt="s.ShortSeasonName" class="card-img-top mx-auto rounded" style="max-width: 250px; max-height: 375px; min-width: 250px; min-height: 375px;">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Season {{s.Number}}</h5>
+                        <p>{{s.EpisodeCount}} episodes</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -21,6 +75,18 @@ export default {
             //Placez les données reçues dans la propriété this.TvShow
             //Complétez..
             //fetch(...
+            let url = `http://tvshowapi.sv55.cmaisonneuve.qc.ca/api/tvshow?TvShowId=${this.TvShowId}`;
+            console.log('url:', url);
+            fetch(url)
+                .then((response) => {
+                    if (!response.ok) {
+                        console.log(response);
+                    }
+                    return response.json();
+                }).then((data) => {
+                    console.log(`data`);
+                    this.TvShow = data;
+                });
         },
     },
     mounted: function () {
