0822b26f33ba3a6a95040c1e2e1b6ef7d754873a
Guillaume Lachance <glachance@cmaisonneuve.qc.ca>
2020-05-14 11:06:23 -0400

    signup, login, profile

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,28 +1,64 @@
 <template>
     <div class="container">
         <h2 style="text-align:center">Sign Up</h2>
-        <form>
+        <form id="signup">
             <div class="form-group row">
-                <label for="email" class="col-sm-3 col-form-label">Email</label>
-                <div class="col-sm-9">
-                    <input type="email" class="form-control" id="email" placeholder="Email"/>
+                <label for="email" class="col-3 col-form-label">Email</label>
+                <div class="col-9">
+                    <input
+                        type="email"
+                        class="form-control"
+                        id="email"
+                        placeholder="Email"
+                        required
+                        maxlength="100"
+                    />
                 </div>
             </div>
             <div class="form-group row">
-                <label for="password" class="col-sm-3 col-form-label">Password</label>
-                <div class="col-sm-9">
-                    <input type="password" class="form-control" id="password" placeholder="Password" />
+                <label for="password" class="col-3 col-form-label">Password</label>
+                <div class="col-9">
+                    <input
+                        type="password"
+                        class="form-control"
+                        id="password"
+                        placeholder="Password"
+                        minlength="4"
+                        maxlength="15"
+                    />
                 </div>
             </div>
             <div class="form-group row">
-                <label for="passwordconfirm" class="col-sm-3 col-form-label">Password Confirmation</label>
-                <div class="col-sm-9">
-                    <input type="password" class="form-control" id="passwordconfirm" placeholder="Password" />
+                <label for="passwordconfirm" class="col-3 col-form-label">Password Confirmation</label>
+                <div class="col-9">
+                    <input
+                        type="password"
+                        class="form-control"
+                        id="passwordconfirm"
+                        placeholder="Password"
+                        minlength="4"
+                        maxlength="15"
+                    />
                 </div>
             </div>
             <div class="form-group row">
-                <div class="col-sm-10">
-                    <button type="submit" v-on:click="SignUp" class="btn btn-primary">Sign up</button>
+                <div class="col-3"></div>
+                <div class="col-9">
+                    <button v-on:click.prevent="SignUp" type="submit" class="btn btn-primary">
+                        SignUp
+                    </button>
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-3"></div>
+                <div id="diverror" class="row">
+                    <div
+                        id="msgerror"
+                        class="alert alert-danger"
+                        style="display:inline-block;margin:0 auto;"
+                        role="alert">
+                        {{ errorText }}
+                    </div>
                 </div>
             </div>
         </form>
@@ -30,13 +66,7 @@
 </template>
 
 <script>
-export default {
-    name: "SignUp",
-    methods: {
-        //est appellée lorsque le bouton SignUp est cliqué
-        SignUp : function () {
-            //appeller la méthode /api/createuser avec les données
-        }
-    }
-};
+    export default {
+        name: "SignUp"
+    };
 </script>
